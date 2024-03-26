df28719ccb75e9c52debc6dd77349f2ba6ef8ba5
drizzer14 <drizzer14@gmail.com>
2024-03-26 13:49:05 +0200

    Updates

---
+++ start.523d62cd.js
@@ -0,0 +1,21 @@
+function e(e,t,r,o){Object.defineProperty(e,t,{get:r,set:o,enumerable:!0,configurable:!0})}var t=globalThis.parcelRequire917c,r=t.register;r("3ukco",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),e(r.exports,"default",()=>t("bMXg0").default),t("bMXg0")}),r("bMXg0",function(r,o){e(r.exports,"default",()=>i);var n=t("ayMG0"),a=t("4dK5w"),i=()=>(0,n.jsxs)(a.Start,{children:[(0,n.jsx)(a.Title,{children:"Stratagem Hero"}),(0,n.jsx)(a.Prompt,{children:"Press any Stratagem Input to Start!"})]})}),r("4dK5w",function(r,o){e(r.exports,"Start",()=>a),e(r.exports,"Title",()=>i),e(r.exports,"Prompt",()=>l);var n=t("lmFrL");let a=(0,n.styled).div`
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`,i=(0,n.styled).h1`
+  margin-bottom: 1em;
+
+  font-size: 108px;
+  font-weight: 700;
+  letter-spacing: -3px;
+  text-transform: uppercase;
+  color: #fff;
+`,l=(0,n.styled).span`
+  font-size: 54px;
+  font-weight: 700;
+  color: var(--brand);
+`});
+//# sourceMappingURL=start.523d62cd.js.map
