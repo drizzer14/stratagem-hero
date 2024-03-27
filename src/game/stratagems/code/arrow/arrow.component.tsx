59452478759ea074e2db77dcda2862e0a7fd1730
drizzer14 <drizzer14@gmail.com>
2024-03-27 10:42:18 +0200

    fix arrow svg attributes

--- src/game/stratagems/code/arrow/arrow.component.tsx
+++ src/game/stratagems/code/arrow/arrow.component.tsx
@@ -12,8 +12,8 @@ const Arrow: FC = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M0.715027 31.8015L31.6459 0.694458L61.707 31.8015V40.515H46.3713V65.9583H16.9198V40.515H0.71283V31.8015H0.715027Z"
         fill="currentColor"
       />
